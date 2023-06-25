fa1e27946daa76d43365aa3f47e6de3d326d6d1e
Ilya_Kargapolov <ilya.kargapolov02@mail.ru>
2023-06-25 19:08:47 +0600

    Реализовано: добавление новостей, чтение новости (по id), получение всех новостей

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('load_all_news/', views.load_all_news_get),
-    path('add_news/', views.add_news_post)
+    path('add_news/', views.add_news_post),
+    path('load_news/<id>', views.load_news_get)
 ]
--- api/views.py
+++ api/views.py
@@ -1,15 +1,20 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import News
+from .serializers import NewsSerializer
 
 @api_view(['GET'])
 def load_all_news_get(request):
-    # Написать сериализатор для возврата всех новостей
-    return Response({'response': 'News'})
+    return Response(News.objects.values())
 
 @api_view(['POST'])
 def add_news_post(request):
     news = News(title=request.data.get('title'), body=request.data.get('body'))
     news.save()
-    # Написать сериализатор для возврата объекта модели
-    return Response({'response': 'News success created'})
+    news_serialized = NewsSerializer(news)
+    return Response({'title': news_serialized.data['title'], 'body': news_serialized.data['body']})
+
+@api_view(['GET'])
+def load_news_get(request, id):
+    news = News.objects.get(id=id)
+    return Response({'title': news.title, 'body': news.body})
